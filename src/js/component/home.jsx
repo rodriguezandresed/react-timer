03f0cb6e5b1830048a1987f31eb36a220606b7e9
Andres Rodriguez <rodriguezandresed@gmail.com>
2022-04-03 00:05:27 -0400

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const icon = <FontAwesomeIcon icon={faClock} />;
 
-//create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container d-flex justify-content-center">
+				<div className=" d-flex justify-content-center p-4 bg-dark text-light rounded w-100">
+					<div className=" px-3 display-2">{icon}</div>
+					<div className=" px-3 mx-3 display-2 border border-light rounded">
+						{props.sextaPos % 10}
+					</div>
+					<div className=" px-3 mx-3 display-2 border border-light rounded">
+						{props.quintaPos % 10}
+					</div>
+					<div className=" px-3 mx-3 display-2 border border-light rounded">
+						{props.cuartaPos % 10}
+					</div>
+					<div className=" px-3 mx-3 display-2 border border-light rounded">
+						{props.terceraPos % 10}
+					</div>
+					<div className=" px-3 mx-3 display-2 border border-light rounded">
+						{props.segundaPos % 10}
+					</div>
+					<div className=" px-3 mx-3 display-2 border border-light rounded">
+						{props.primeraPos % 10}
+					</div>
+				</div>
+			</div>
+		</>
 	);
+};
+
+Home.propTypes = {
+	props: PropTypes.number,
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,28 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(() => {
+	const six = Math.floor(counter / 100000);
+	const five = Math.floor(counter / 10000);
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	counter++;
+	ReactDOM.render(
+		<Home
+			primeraPos={one}
+			segundaPos={two}
+			terceraPos={three}
+			cuartaPos={four}
+			quintaPos={five}
+			sextaPos={six}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
